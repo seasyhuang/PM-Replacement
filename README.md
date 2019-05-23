d793b7fb9d34805b7ebaa7c7fd7319232aad2ead
Seasy <seasy@Seasys-MacBook-Pro.local>
2019-05-23 11:01:10 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pm-replacement
 auto practice scheduler
+
+answers to questions that i had:
+- https://stackoverflow.com/questions/419163/what-does-if-name-main-do#419185
---
+++ scheduler.py
@@ -0,0 +1,23 @@
+# Script to generate practice schedules based on member avails
+# Takes member avails (array of 7) as input
+# Studio avails similar to member avails as input
+
+import sys
+
+var1 = args[1]
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def helper1(var1):
+    """Return a list of tuples containing the top grossing movies of 2013 and link to their IMDB
+    page."""
+    pass
+
+def helper2(var1):
+    """Return a list of keywords associated with *movie*."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
